e7942ffc1af20f45eceee629789b7645560ac8ae
Cal3123 <tettehcaleb@gmail.com>
2022-03-03 08:42:12 -0500

    Added Authors And Books CSS Styles

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,17 +1,31 @@
-//registerPlugin method allows plugins (in layout page) to be registered with Filepond
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+const  rootStyles = window.getComputedStyle(document.documentElement)
 
-//allows us to setOptions for all our filepond instances. Method of filpond object
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 /  100,
-    //below are properties of image-resize plugin of filepond
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if(rootStyles.getPropertyValue("--book-cover-width-large") != null 
+    && rootStyles.getPropertyValue("--book-cover-width-large") !== ""){
+    ready()
+} else {
+    document.getElementById("main-css").addEventListener("load", ready)
+}
 
-//turns all file inputs in our entire page into filepond inputs
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue("--book-cover-width-large"))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue("--book-cover-aspect-ratio"))
+    const coverHeight = coverWidth/coverAspectRatio
+    //registerPlugin method allows plugins (in layout page) to be registered with Filepond
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+
+    //allows us to setOptions for all our filepond instances. Method of filpond object
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1/coverAspectRatio,
+        //below are properties of image-resize plugin of filepond
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+
+    //turns all file inputs in our entire page into filepond inputs
+    FilePond.parse(document.body);
+}
